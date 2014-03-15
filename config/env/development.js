8005acd07bf95d3ea573e7edf719f6b520c878d3
clghks <clghks@gmail.com>
2014-03-15 14:41:59 +0900

    프로젝트 생성

---
+++ config/env/development.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+    db: 'mongodb://localhost/mean-dev',
+    app: {
+        name: 'MEAN - A Modern Stack - Development'
+    },
+    facebook: {
+        clientID: 'APP_ID',
+        clientSecret: 'APP_SECRET',
+        callbackURL: 'http://localhost:3000/auth/facebook/callback'
+    },
+    twitter: {
+        clientID: 'CONSUMER_KEY',
+        clientSecret: 'CONSUMER_SECRET',
+        callbackURL: 'http://localhost:3000/auth/twitter/callback'
+    },
+    github: {
+        clientID: 'APP_ID',
+        clientSecret: 'APP_SECRET',
+        callbackURL: 'http://localhost:3000/auth/github/callback'
+    },
+    google: {
+        clientID: '524603440707.apps.googleusercontent.com',
+        clientSecret: '2bnjdfQetkd29Y6G4K-IhC9N',
+        callbackURL: 'http://localhost:3000/auth/google/callback'
+    },
+    linkedin: {
+        clientID: 'API_KEY',
+        clientSecret: 'SECRET_KEY',
+        callbackURL: 'http://localhost:3000/auth/linkedin/callback'
+    }
+};
